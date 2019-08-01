db61e9e019ac517f684a50ac89a7084d7874cb27
tsingsonqin <tsingson@gmail.com>
2019-08-01 21:00:30 +0800

    fix

--- README.md
+++ README.md
@@ -1,1 +1,32 @@
 # uber-bo/zap logger for fasthttp and glog ....
+
+
+
+## zap log to console for debug make easy
+
+```
+package main
+
+import (
+	"go.uber.org/zap"
+
+	"github.com/tsingson/zaplogger"
+)
+
+func main() {
+	core := zaplogger.NewConsoleDebug()
+
+	// From a zapcore.Core, it's easy to construct a Logger.
+	logger := zap.New(core)
+	defer logger.Sync()
+	logger.Info("constructed a logger")
+}
+
+```
+
+output
+
+```
+2019-08-01T20:59:41.879+0800    INFO    constructed a logger
+
+```
--- example/main.go
+++ example/main.go
@@ -7,21 +7,10 @@ import (
 )
 
 func main() {
-
-	// The bundled Config struct only supports the most common configuration
-	// options. More complex needs, like splitting logs between multiple files
-	// or writing to non-file outputs, require use of the zapcore package.
-	//
-	// In this example, imagine we're both sending our logs to Kafka and writing
-	// them to the console. We'd like to encode the console output and the Kafka
-	// topics differently, and we'd also like special treatment for
-	// high-priority logs.
-
 	core := zaplogger.NewConsoleDebug()
 
 	// From a zapcore.Core, it's easy to construct a Logger.
 	logger := zap.New(core)
 	defer logger.Sync()
 	logger.Info("constructed a logger")
-
 }
